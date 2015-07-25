b9d3b64830380fa1572262400e0c5c61833a1082
Thomas Moore <tsanm93@gmail.com>
2015-07-25 15:34:25 +0100

    Switch to my(works) coding standards

--- app/Http/Controllers/ZenThoughtController.php
+++ app/Http/Controllers/ZenThoughtController.php
@@ -6,36 +6,36 @@ use App\Thought;
 
 class ZenThoughtController extends Controller
 {
-    /**
-     * Show the thought for today.
-     *
-     * @return Response
-     */
-    public function showTodayThought()
-    {
-        $day = date('d');
-        $thought = Thought::findOrFail($day);
+	/**
+	 * Show the thought for today.
+	 *
+	 * @return Response
+	 */
+	public function showTodayThought()
+	{
+		$day = date('d');
+		$thought = Thought::findOrFail($day);
 
-        return response()->json([
-            "day" => $day,
-            "thought_jp" => $thought->thought_jp,
-            "thought_en" => $thought->thought_en
-        ]);
-    }
+		return response()->json([
+			"day" => $day,
+			"thought_jp" => $thought->thought_jp,
+			"thought_en" => $thought->thought_en
+		]);
+	}
 
-    /**
-     * Show the thought for the given day.
-     *
-     * @param  int  $day
-     * @return Response
-     */
-    public function showDayThought($day)
-    {
-        $thought = Thought::findOrFail($day);
-        return response()->json([
-            "day" => $day,
-            "thought_jp" => $thought->thought_jp,
-            "thought_en" => $thought->thought_en
-        ]);
-    }
+	/**
+	 * Show the thought for the given day.
+	 *
+	 * @param  int  $day
+	 * @return Response
+	 */
+	public function showDayThought($day)
+	{
+		$thought = Thought::findOrFail($day);
+		return response()->json([
+			"day" => $day,
+			"thought_jp" => $thought->thought_jp,
+			"thought_en" => $thought->thought_en
+		]);
+	}
 }
